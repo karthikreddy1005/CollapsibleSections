62b2281d9eeb86a8cfdc6e6bf8c2add02016c07c
karthikreddy1005 <karthikb10005@gmail.com>
2024-04-17 20:26:00 +0530

    base code

--- CollapsibleCollectionView/CollapsibleCollectionView/ViewController.swift
+++ CollapsibleCollectionView/CollapsibleCollectionView/ViewController.swift
@@ -11,9 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .lightGray
         // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    lazy var mainButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("click for new VC", for: .normal)
+        button.backgroundColor = .green
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func buttonClicked() {
+        
     }
 
+    func setupViews() {
+        self.view.addSubview(mainButton)
+        NSLayoutConstraint.activate([
+            mainButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 
 }
 
