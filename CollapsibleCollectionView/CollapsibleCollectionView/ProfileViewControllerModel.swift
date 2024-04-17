c0f75f994cbe004f09fbbe92213823bf36233102
karthik-zee <karthik.reddy@zee.com>
2024-04-17 23:45:55 +0530

    commit from different laptop

--- CollapsibleCollectionView/CollapsibleCollectionView/ProfileViewControllerModel.swift
+++ CollapsibleCollectionView/CollapsibleCollectionView/ProfileViewControllerModel.swift
@@ -22,8 +22,11 @@ class ProfileViewControllerModel {
     let sectionsData: [ProfileSection] = [
         ProfileSection(type: .small, itemCount: 10, title: "Small Section"),
         ProfileSection(type: .large, itemCount: 5, title: "Large Section"),
-        ProfileSection(type: .small, itemCount: 2, title: "third section"),
-        ProfileSection(type: .small, itemCount: 1, title: "fourth section")
+//        ProfileSection(type: .small, itemCount: 2, title: "third section"),
+//        ProfileSection(type: .small, itemCount: 1, title: "fourth section"),
+//        ProfileSection(type: .small, itemCount: 3, title: "fifth section"),
+//        ProfileSection(type: .small, itemCount: 5, title: "sixth section"),
+        ProfileSection(type: .small, itemCount: 6, title: "seventh section"),
         // Add more sections as needed
     ]
 }
